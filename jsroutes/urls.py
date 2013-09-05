42fda81e586e216fbcd191e67c87ad3d8e44487c
Igor Tokarev <TigorC@gmail.com>
2013-09-05 12:25:55 +0600

    Support for django 1.5 (delete unused "direct_to_template" from urls.py)

--- jsroutes/urls.py
+++ jsroutes/urls.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
+from django.conf.urls.defaults import patterns, url
 
-from django.conf.urls.defaults import patterns, include, url
-from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns("jsroutes.views",
     url(r"^routes\.js$", "routes", name="jsroutes"),
